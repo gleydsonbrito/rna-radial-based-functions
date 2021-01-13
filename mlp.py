d3c992e0712ebe06640c51a15d679cd168fb6894
gleydsonbrito <gleydsonbrito@hotmail.com>
2021-01-13 18:24:49 -0300

    add some features on MLPClassifier

--- mlp.py
+++ mlp.py
@@ -7,24 +7,23 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 
 profile_train = pd.read_csv("RBF/src/treinamento2.csv")
-#rofile_validation = pd.read_csv("RBF/src/validacao.csv")
 
-X = profile_train.iloc[:, 0:21]
+X = profile_train[profile_train.columns[:-1]]
 
-Y = profile_train.iloc[:, 21:22]
+Y = profile_train[profile_train.columns[-1]]
 
 X.head()
 Y.head()
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.10)
 
 scaler = StandardScaler()
 scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-mlp = MLPClassifier(hidden_layer_sizes=(100, 250, 150, 10),
-                    max_iter=50000)
+mlp = MLPClassifier(hidden_layer_sizes=(40, 60, 25, 10), random_state=1, activation='identity',
+                    solver='adam', max_iter=5000, learning_rate_init=0.0015, learning_rate='adaptive')
 mlp.fit(X_train, Y_train.values.ravel())
 
 predictions = mlp.predict(X_test)
